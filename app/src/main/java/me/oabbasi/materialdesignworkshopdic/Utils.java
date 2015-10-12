6b1b5f95dee19a531f1cfa48ac631d7d5666d93b
oubai <oubai.abbasi@gmail.com>
2015-10-12 18:43:42 +0400

    Added main Utils methods

---
+++ app/src/main/java/me/oabbasi/materialdesignworkshopdic/Utils.java
@@ -0,0 +1,43 @@
+package me.oabbasi.materialdesignworkshopdic;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+/**
+ * This class is used to share commonly used methods
+ * Created by oubai on 10/12/15.
+ */
+public class Utils {
+    public static ArrayList<String> getNotes(Context context) {
+        return null;
+    }
+
+    public static void addNote(Context context) {
+
+    }
+
+    public static void deleteNote(Context context, String noteTitle) {
+
+    }
+
+    public static boolean noteExists(Context context, String noteTitle) {
+        return false;
+    }
+
+    public static ArrayList<String> getNoteItems(Context context, String noteTitle) {
+        return null;
+    }
+
+    public static void addNoteItem(Context context, String noteTitle, String item) {
+
+    }
+
+    public static void deleteNoteItem(Context context, String noteTitle, String item) {
+
+    }
+
+    public static boolean noteItemExists(Context context, String noteTitle, String item) {
+        return false;
+    }
+}
